7560eda6d1c7db7a3dbe55782a8a0b2241e70a6e
Gameking1happy <gameking1happy@icloud.com>
2025-10-27 20:00:25 -0400

    Edited mixins to reduce spam

--- common/src/main/java/io/github/gameking1happy/gk1hcore/mixin/MixinMinecraft.java
+++ common/src/main/java/io/github/gameking1happy/gk1hcore/mixin/MixinMinecraft.java
@@ -1,6 +1,5 @@
 package io.github.gameking1happy.gk1hcore.mixin;
 
-import io.github.gameking1happy.gk1hcore.GK1HCore;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +11,5 @@ public class MixinMinecraft {
     
     @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) {
-        // Logs the Minecraft version type.
-        GK1HCore.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
     }
 }
--- neoforge/src/main/java/io/github/gameking1happy/gk1hcore/mixin/MixinTitleScreen.java
+++ neoforge/src/main/java/io/github/gameking1happy/gk1hcore/mixin/MixinTitleScreen.java
@@ -1,7 +1,5 @@
 package io.github.gameking1happy.gk1hcore.mixin;
 
-import io.github.gameking1happy.gk1hcore.GK1HCore;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +11,5 @@ public class MixinTitleScreen {
 
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        // Logs the Minecraft version type.
-        GK1HCore.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
     }
 }
