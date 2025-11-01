94d94da0f9a3e3d7617d27afb7b5890ec524c266
Gameking1happy <gameking1happy@icloud.com>
2025-11-01 15:33:18 -0400

    Experimental changes of configMaker.

--- common/src/main/java/io/github/gameking1happy/gk1hcore/config/ConfigMaker.java
+++ common/src/main/java/io/github/gameking1happy/gk1hcore/config/ConfigMaker.java
@@ -1,9 +1,12 @@
 package io.github.gameking1happy.gk1hcore.config;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import static net.neoforged.neoforge.common.ModConfigSpec.*;
 public class ConfigMaker {
     // A method to reduce boilerplate when making config values.
-    public static <T> ConfigValue<T> makeConfig(Builder BUILDER, String translationKey, String comment, String name, T defaultValue) {
+    public static <T> ConfigValue<T> makeConfig(@NotNull Builder BUILDER, @NotNull String name, @NotNull T defaultValue, @Nullable String comment, @Nullable String translationKey) {
         return BUILDER
                 .translation(translationKey)
                 .comment(comment)
