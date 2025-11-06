8e19ad40df4f7669bc10e33770e5ead822b14efe
Gameking1happy <gameking1happy@icloud.com>
2025-11-06 09:46:05 -0500

    dev

--- fabric/src/main/java/io/github/gameking1happy/gk1hcore/MainFabric.java
+++ fabric/src/main/java/io/github/gameking1happy/gk1hcore/MainFabric.java
@@ -2,15 +2,9 @@ package io.github.gameking1happy.gk1hcore;
 
 import net.fabricmc.api.ModInitializer;
 
-import static io.github.gameking1happy.gk1hcore.platform.Services.PLATFORM;
-
 public class MainFabric implements ModInitializer {
 
     public void onInitialize() {
-        // Ensures that the Forge Config API Port is loaded, probably unnecessary but just in case.
-        if (!PLATFORM.isModLoaded("forgeconfigapiport")) {
-            throw new IllegalArgumentException("FCAP IS NOT LOADED!!!");
-        }
         // Initialize the mod.
         Main.init();
     }
