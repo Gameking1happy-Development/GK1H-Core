90ac52b99a01debbd46e69ee651bc0fdf28f1954
Gameking1happy <gameking1happy@icloud.com>
2025-12-13 11:26:46 -0500

    Fixed constant names that incorrectly used camelCase instead of SCREAMING_SNAKE_CASE.

--- 1.21.1/common/src/main/java/io/github/gameking1happy/gk1hcore/compat/CompatFlags.java
+++ 1.21.1/common/src/main/java/io/github/gameking1happy/gk1hcore/compat/CompatFlags.java
@@ -10,65 +10,65 @@ public class CompatFlags {
     /**
      * Metal Bundles
      */
-    public static final boolean metBun = PLATFORM.isModLoaded("metalbundles");
+    public static final boolean MET_BUN = PLATFORM.isModLoaded("metalbundles");
     /**
      * Netherited
      */
-    public static final boolean neted = PLATFORM.isModLoaded("netherited");
+    public static final boolean NETED = PLATFORM.isModLoaded("netherited");
     /**
      * Universal Enchants
      */
-    public static final boolean uniEnc = PLATFORM.isModLoaded("universalenchants");
+    public static final boolean UNI_ENC = PLATFORM.isModLoaded("universalenchants");
     /**
      * Spiky Spikes
      */
-    public static final boolean spiSpi = PLATFORM.isModLoaded("spikyspikes");
+    public static final boolean SPI_SPI = PLATFORM.isModLoaded("spikyspikes");
     /**
      * Advanced Netherite
      */
-    public static final boolean advNet = PLATFORM.isModLoaded("advancednetherite");
+    public static final boolean ADV_NET = PLATFORM.isModLoaded("advancednetherite");
     /**
      * Accessories
      */
-    public static final boolean access = PLATFORM.isModLoaded("accessories");
+    public static final boolean ACCESS = PLATFORM.isModLoaded("accessories");
     /**
      * Accessories Compat Layer
      */
-    public static final boolean accCom = PLATFORM.isModLoaded("accessories_compat_layer");
+    public static final boolean ACC_COM = PLATFORM.isModLoaded("accessories_compat_layer");
     /**
      * Curios
      */
-    public static final boolean curi = PLATFORM.isModLoaded("curios");
+    public static final boolean CURI = PLATFORM.isModLoaded("curios");
     /**
      * Trinkets
      */
-    public static final boolean trink = PLATFORM.isModLoaded("trinkets");
+    public static final boolean TRINK = PLATFORM.isModLoaded("trinkets");
     /**
      * Reinforced Shulker Boxes
      */
-    public static final boolean reinfShu = PLATFORM.isModLoaded("reinfshulker");
+    public static final boolean REINF_SHU = PLATFORM.isModLoaded("reinfshulker");
     /**
      * GK1H Tags
      */
-    public static final boolean gk1hTags = PLATFORM.isModLoaded("gk1htags");
+    public static final boolean GK1H_TAGS = PLATFORM.isModLoaded("gk1htags");
     /**
      * Netherite Extras
      */
-    public static final boolean netExt = PLATFORM.isModLoaded("netheriteextras");
+    public static final boolean NET_EXT = PLATFORM.isModLoaded("netheriteextras");
     /**
      * Enchanting Infuser
      */
-    public static final boolean encInf = PLATFORM.isModLoaded("enchantinginfuser");
+    public static final boolean ENC_INF = PLATFORM.isModLoaded("enchantinginfuser");
     /**
      * GK1H Recipes
      */
-    public static final boolean gk1hRecipes = PLATFORM.isModLoaded("gk1hrecipes");
+    public static final boolean GK1H_RECIPES = PLATFORM.isModLoaded("gk1hrecipes");
     /**
      * Sword Blocking Mechanics
      */
-    public static final boolean swoBloMec = PLATFORM.isModLoaded("swordblockingmechanics");
+    public static final boolean SWO_BLO_MEC = PLATFORM.isModLoaded("swordblockingmechanics");
     /**
      * Ender Zoology
      */
-    public static final boolean endZoo = PLATFORM.isModLoaded("enderzoology");
+    public static final boolean END_ZOO = PLATFORM.isModLoaded("enderzoology");
 }
